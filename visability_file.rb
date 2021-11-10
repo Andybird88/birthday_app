cb318eec69144270bf7ade7492d3bba58fb140a2
88Andybird <anbird88@gmail.com>
2021-11-10 13:20:55 +0000

    used rubocop to format

--- visability_file.rb
+++ visability_file.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
-#This was a working file to see visability of some of the code.
-#Have since deleted the contents as most is no longer valid.
-
+# This was a working file to see visability of some of the code.
+# Have since deleted the contents as most is no longer valid.
